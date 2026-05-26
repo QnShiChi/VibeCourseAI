ef425c9b2308a477f772d719b2f2fb888e525bcf
gagaduy <duongvanduy799@gmail.com>
2026-05-26 21:45:15 +0700

    Chuyen qua ASP.NET

---
+++ backups/vibe_course_ai_db_20260526_214453.sql
@@ -0,0 +1,4 @@
+-- Generated restore helper for vibe_course_ai_db
+-- Preferred full restore artifact: vibe_course_ai_db_20260526_214453.bak
+-- Use: make restore FILE=backups/vibe_course_ai_db_20260526_214453.bak
+SELECT 'vibe_course_ai_db' AS database_name, 'vibe_course_ai_db_20260526_214453.bak' AS backup_file;

5a7c29e93f5cb84e5611099036f71d251a2bf84f
Phan Duong Quoc Nhat <pdqnshichi2005@gmail.com>
2026-05-28 20:15:50 +0700

    backup db after fixing video bugs

--- backups/vibe_course_ai_db_20260526_224236.sql
+++
@@ -1,4 +0,0 @@
--- Generated restore helper for vibe_course_ai_db
--- Preferred full restore artifact: vibe_course_ai_db_20260526_224236.bak
--- Use: make restore FILE=backups/vibe_course_ai_db_20260526_224236.bak
-SELECT 'vibe_course_ai_db' AS database_name, 'vibe_course_ai_db_20260526_224236.bak' AS backup_file;
---
+++ backups/vibe_course_ai_db_20260528_201513.sql
@@ -0,0 +1,4 @@
+-- Generated restore helper for vibe_course_ai_db
+-- Preferred full restore artifact: vibe_course_ai_db_20260528_201513.bak
+-- Use: make restore FILE=backups/vibe_course_ai_db_20260528_201513.bak
+SELECT 'vibe_course_ai_db' AS database_name, 'vibe_course_ai_db_20260528_201513.bak' AS backup_file;
